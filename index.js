abfcc7010f73d34e4f212dcf9309365b72da46db
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-05-01 18:45:13 +0600

    vercel done

--- index.js
+++ index.js
@@ -5,16 +5,40 @@ const port = process.env.port || 5000;
 
 
 const categories = require('./data/categories.json')
-
+const news = require('./data/news.json')
 app.use(cors())
 
 app.get('/', (req, res)=>{
-    res.send("Hello you get your data")
+    res.send("Hello you get your data");
 })
 
 
 app.get('/categories', (req, res)=>{
-    res.send(categories)
+    res.send(categories);
+})
+
+app.get('/news', (req, res)=>{
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res)=>{
+    const id = req.params.id;
+    const selected = news.find(n => n._id === id);
+    res.send(selected);
+    // console.log(selected);
+})
+
+app.get('/categories/:id', (req, res)=>{
+    const id = req.params.id;
+    // console.log(typeof Number(id))
+    if(Number(id) === 0){
+        res.send(news)
+    }
+    else{
+        const selectedCategories = news.filter(n => n.category_id === id);
+        res.send(selectedCategories);
+    }
+    
 })
 
 
